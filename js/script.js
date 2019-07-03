af344e6ed06393f7f73204d9d5265714900424fa
Evgeny <jeng-009@yandex.ru>
2019-07-03 12:26:13 +0300

    Финал (надеюсь)

--- js/script.js
+++ js/script.js
@@ -1,81 +1,62 @@
 
-var write = document.querySelector(".location-info-button");
-var writeForm = document.querySelector(".write-us-form");
-var writeClose = writeForm.querySelector(".form-button-close");
-var writeName = writeForm.querySelector("[name=username]");
-var writeEmail = writeForm.querySelector("[name=email]");
 
-var storageName = "";
+var link = document.querySelector(".location-info-button");
+var popup = document.querySelector(".write-us-form");
+var close = popup.querySelector(".form-button-close");
+
+var login = popup.querySelector("[name=login]");
+var mail = popup.querySelector("[name=email]");
+var message = popup.querySelector(".write-us-text");
+
 var isStorageSupport = true;
+var storage = "";
 
 try {
-  storageName = localStorage.getItem("writeName");
-} catch (err) {
+  storage = localStorage.getItem("login");
+}
+catch (err) {
   isStorageSupport = false;
 }
 
-write.addEventListener("click", function(evt) {
+link.addEventListener ("click", function (evt) {
   evt.preventDefault();
-  writeForm.classList.add("write-form-show");
-  if (storageName) {
-    writeName.value = storageName;
-    writeEmail.focus();
-  } else {
-    writeName.focus();
-  }
-});
+  popup.classList.add("modal-show");
+  login.focus();
 
-writeClose.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  writeForm.classList.remove("write-form-show");
-  writeForm.classList.remove("write-form-error")
-  writeName.classList.remove("form-input-animation");
-  writeEmail.classList.remove("form-input-animation");
-});
-
-writeName.onfocus = function() {
-  if (writeName.classList.contains("form-input-animation")) {
-    writeName.classList.remove("form-input-animation");
-  }
-};
-
-writeEmail.onfocus = function() {
-  if (writeEmail.classList.contains("form-input-animation")) {
-    writeEmail.classList.remove("form-input-animation");
-  }
-};
-
-writeForm.addEventListener("submit", function(evt) {
-  if (!writeName.value || !writeEmail.value) {
-    evt.preventDefault();
-    writeForm.classList.remove("write-form-error")
-    writeForm.offsetWidth = writeForm.offsetWidth;
-    writeForm.classList.add("write-form-error")
-    console.log("Нужно ввести логи и пароль");
-      if(!writeName.value) {
-        writeName.classList.remove("form-input-animation");
-        writeName.offsetWidth = writeName.offsetWidth;
-        writeName.classList.add("form-input-animation");
-      } if (!writeEmail.value) {
-        writeEmail.classList.remove("form-input-animation");
-        writeEmail.offsetWidth = writeEmail.offsetWidth;
-        writeEmail.classList.add("form-input-animation");
-      }
-  } else {
-    if (isStorageSupport) {
-    localStorage.setItem("writeName", writeName.value);
+   if (storage) {
+      login.value = storage;
+      mail.focus();
+    } else {
+      login.focus();
     }
-  }
 });
 
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (writeForm.classList.contains("write-form-show")) {
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+popup.addEventListener("submit", function (evt) {
+  if (!login.value || !mail.value || !message.value) {
       evt.preventDefault();
-      writeForm.classList.remove("write-form-show");
-      writeForm.classList.remove("write-form-error")
-      writeName.classList.remove("form-input-animation");
-      writeEmail.classList.remove("form-input-animation");
+      popup.classList.remove ("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+  } 
+  else {
+    if (isStorageSupport) {
+    localStorage.setItem("mail", mail.value);
+  }
+}
+});
+
+window.addEventListener ("keydown", function (evt) {
+  if (evt.keyCode == 27) {
+    evt.preventDefault ();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
     }
   }
 });
